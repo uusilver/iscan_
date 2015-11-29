86cb3fe1af64377a5af852fbefda587fe7fe74f3
vani_junying@hotmail.com <vani_junying@hotmail.com>
2015-11-29 16:54:06 +0800

    完成充值和统计

--- src/main/java/com/tmind/iscan/service/UserAccountService.java
+++ src/main/java/com/tmind/iscan/service/UserAccountService.java
@@ -1,6 +1,7 @@
 package com.tmind.iscan.service;
 
 import com.tmind.iscan.entity.M_USER_ACCOUNT;
+import com.tmind.iscan.entity.M_USER_ACCOUNT_OPT;
 import com.tmind.iscan.entity.M_USER_PRODUCT_ENTITY;
 import com.tmind.iscan.util.HibernateUtil;
 import org.hibernate.Query;
@@ -58,5 +59,78 @@ public class UserAccountService {
             }
         }
         return flag;
+    }
+
+    public M_USER_ACCOUNT queryAccountForDisplay(Integer userId){
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        M_USER_ACCOUNT mUserAccount = null;
+        try {
+            String hql = "from M_USER_ACCOUNT as M_USER_ACCOUNT where M_USER_ACCOUNT.user_id=:userId";//使用命名参数，推荐使用，易读。
+            Query query = session.createQuery(hql);
+            query.setInteger("userId", userId);
+            mUserAccount = (M_USER_ACCOUNT)query.list().get(0);
+        }finally {
+            if(session!=null){
+                session.close();
+            }
+        }
+        return mUserAccount;
+    }
+
+    //为充值二维码更新
+    public boolean purchaseQrAmountAndKeepRecordIntoOptTable(M_USER_ACCOUNT account, M_USER_ACCOUNT_OPT opt){
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tran = session.beginTransaction();
+        try{
+            session.update(account);
+            session.save(opt);
+            tran.commit();
+            return true;
+        }catch (Exception e ){
+            tran.rollback();
+            System.out.println(e.getMessage());
+            return false;
+        }finally {
+            if(session!=null){
+                session.close();
+
+            }
+        }
+    }
+
+    //单独为了消费二维码更新
+    public boolean purchaseQrAmountAndKeepRecordIntoOptTable(M_USER_ACCOUNT_OPT opt){
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tran = session.beginTransaction();
+        try{
+            session.save(opt);
+            tran.commit();
+            return true;
+        }catch (Exception e ){
+            tran.rollback();
+            System.out.println(e.getMessage());
+            return false;
+        }finally {
+            if(session!=null){
+                session.close();
+
+            }
+        }
+    }
+
+    public List<M_USER_ACCOUNT_OPT> queryUserOpt(Integer userId){
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        List<M_USER_ACCOUNT_OPT> list = null;
+        try {
+            String hql = "from M_USER_ACCOUNT_OPT as M_USER_ACCOUNT_OPT where M_USER_ACCOUNT_OPT.user_id=:userId";//使用命名参数，推荐使用，易读。
+            Query query = session.createQuery(hql);
+            query.setInteger("userId", userId);
+            list = query.list();
+        }finally {
+            if(session!=null){
+                session.close();
+            }
+        }
+        return list;
     }
 }
